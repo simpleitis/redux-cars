babb331295483540f6d883a559affc60f8bc338b
Amar <amarkrishna25@gmail.com>
2023-02-06 12:31:41 +0530

    first commit

---
+++ src/components/CarForm.js
@@ -0,0 +1,57 @@
+import React from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { addCar, changeCost, changeName } from '../store';
+
+function CarForm() {
+    const dispatch = useDispatch();
+    const { name, cost } = useSelector((state) => {
+        return state.form;
+    });
+
+    const handleNameChange = (event) => {
+        dispatch(changeName(event.target.value));
+    };
+
+    const handleCostChange = (event) => {
+        const carCost = parseInt(event.target.value) || 0;
+        dispatch(changeCost(carCost));
+    };
+
+    const handleSubmit = (event) => {
+        event.preventDefault();
+
+        dispatch(addCar({ name, cost }));
+    };
+
+    return (
+        <div className="car-form panel">
+            <h4 className="subtitle is-3">Add car</h4>
+            <form onSubmit={handleSubmit}>
+                <div className="field-group">
+                    <div className="field">
+                        <label className="label">Name</label>
+                        <input
+                            className="input is-expanded"
+                            value={name}
+                            onChange={handleNameChange}
+                        />
+                    </div>
+                    <div className="field">
+                        <label className="label">Cost</label>
+                        <input
+                            type="number"
+                            className="input is-expanded"
+                            value={cost || ''}
+                            onChange={handleCostChange}
+                        />
+                    </div>
+                    <div className="field">
+                        <button className="button is-link">Submit</button>
+                    </div>
+                </div>
+            </form>
+        </div>
+    );
+}
+
+export default CarForm;
